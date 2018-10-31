2564cc89f3db09f35fd7c88b264c8edf1e37afd8
xiaoyuan <seyooe@gmail.com>
2018-10-31 16:56:15 +0800

    Color adaptive statusBar style according to scrollView background color.

--- UIScrollView+NoDataExtend.m
+++ UIScrollView+NoDataExtend.m
@@ -13,6 +13,53 @@ typedef NSString * ImplementationKey NS_EXTENSIBLE_STRING_ENUM;
 
 static const CGFloat NoDataPlaceholderHorizontalSpaceRatioValue = 16.0;
 
+@implementation UIColor (ERRgbExtensions)
+
+/// 获取RGB值
++ (void)getRGBComponents:(CGFloat [3])components forColor:(UIColor *)color {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_6_1
+    int bitmapInfo = kCGBitmapByteOrderDefault | kCGImageAlphaPremultipliedLast;
+#else
+    int bitmapInfo = kCGImageAlphaPremultipliedLast;
+#endif
+    
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char resultingPixel[4];
+    CGContextRef context = CGBitmapContextCreate(&resultingPixel,
+                                                 1,
+                                                 1,
+                                                 8,
+                                                 4,
+                                                 rgbColorSpace,
+                                                 bitmapInfo);
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, CGRectMake(0, 0, 1, 1));
+    CGContextRelease(context);
+    CGColorSpaceRelease(rgbColorSpace);
+    
+    for (int component = 0; component < 3; component++) {
+        components[component] = resultingPixel[component];
+    }
+}
+
+
+/// 判断颜色是不是亮色
++ (BOOL)isLightColor:(UIColor *)color {
+    CGFloat components[3];
+    [UIColor getRGBComponents:components forColor:color];
+    //    NSLog(@"%f %f %f", components[0], components[1], components[2]);
+    
+    CGFloat num = components[0] + components[1] + components[2];
+    if (num < 382) {
+        return NO;
+    }
+    else {
+        return YES;
+    }
+}
+
+@end
+
 #pragma mark *** _WeakObjectContainer ***
 
 @interface _WeakObjectContainer : NSObject
@@ -271,9 +318,22 @@ static const CGFloat NoDataPlaceholderHorizontalSpaceRatioValue = 16.0;
         view = self.customNoDataView();
     }
     else if (self.xy_loading) {
-        UIActivityIndicatorView *activityView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        
+        UIView *view = self;
+        UIColor *backgroundColor = view.backgroundColor;
+        while (view == nil) {
+            view = view.superview;
+            backgroundColor = view.backgroundColor;
+            if (backgroundColor != nil) {
+                break;
+            }
+        }
+        
+        BOOL isLightColor = [UIColor isLightColor:backgroundColor];
+        UIActivityIndicatorView *activityView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:isLightColor?UIActivityIndicatorViewStyleGray:UIActivityIndicatorViewStyleWhite];
         [activityView startAnimating];
-        return activityView;
+        view = activityView;
+        return view;
     }
     if (view) {
         NSParameterAssert([view isKindOfClass:[UIView class]]);
